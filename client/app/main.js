8f37f513f91237d39e623480cd040d8e0de3e80e
chris-hildebrandt <ch.hild15@gmail.com>
2022-08-12 09:49:54 -0600

    Co-authored-by: Garrett Adams <garrett-adamss@users.noreply.github.com> Co-authored-by: Zach Rasmussen <zachrasmussen@users.noreply.github.com>

--- client/app/main.js
+++ client/app/main.js
@@ -1,9 +1,14 @@
 import { AuthController } from './Controllers/AuthController.js'
 import { ValuesController } from './Controllers/ValuesController.js'
+import { FeedController } from './Controllers/FeedController.js'
 
 class App {
   authController = new AuthController();
   valuesController = new ValuesController();
+
+  feedController = new FeedController();
+
+  postController = new PostController
 }
 
 // @ts-ignore
