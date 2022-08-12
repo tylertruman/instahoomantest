7dfaedcae85857fecbcddaddeb03f0187acdc66c
chris-hildebrandt <ch.hild15@gmail.com>
2022-08-12 15:02:38 -0600

    Co-authored-by: Garrett Adams <garrett-adamss@users.noreply.github.com> Co-authored-by: Zach Rasmussen <zachrasmussen@users.noreply.github.com> Co-authored-by: ChristineKlosterman <ChristineKlosterman@users.noreply.github.com> Co-authored-by: Tyler Truman <tylertruman@users.noreply.github.com>

--- client/app/Services/PostsService.js
+++ client/app/Services/PostsService.js
@@ -4,7 +4,7 @@ import { api } from "./AxiosService.js";
 class PostsService {
   async deletePost(postId) {
     let res = await api.delete(`api/post/${postId}`)
-    ProxyState.post = ProxyState.post.filter(p => p.id != postId)
+    ProxyState.posts = ProxyState.posts.filter(p => p.id != postId)
     
   } 
 
